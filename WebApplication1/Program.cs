7c389c52fb85aba9e6c9c1ef9202013e50e24635
Trevor-Koenig <trevorkoenig4@gmail.com>
2022-10-13 13:28:06 -0700

    Add Roles features in program.cs

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -11,10 +11,21 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddRoles<IdentityRole>()
     .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var DB = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+    var um = scope.ServiceProvider.GetRequiredService<UserManager<IdentityUser>>();
+    var rm = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+
+    //await DB.InitializeUsers(um, rm);
+    //await DB.InitializeApplications(um);
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
